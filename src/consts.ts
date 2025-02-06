37df3a3b1bf787afd10a6947e273324437864f5f
Outcast-Events <cowboyfun09@gmail.com>
2025-02-06 09:11:35 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
+come// Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Outcasts Events";
+export const SITE_DESCRIPTION = "Hello, And To My Website! Outcasts Events";
